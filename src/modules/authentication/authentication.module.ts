0a2e41ab2560e7377f47de4a311784aa901d7b93
[CORIZE] Tan Le <tan.b4pt.le@misumi.co.jp>
2023-08-20 12:02:00 +0700

    Validate refresh token

--- src/modules/authentication/authentication.module.ts
+++ src/modules/authentication/authentication.module.ts
@@ -1,16 +1,23 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { JwtModule, JwtService } from '@nestjs/jwt';
-import { PassportModule } from '@nestjs/passport';
-import { UserModule } from 'src/modules/user/user.module';
-import { AuthenticationController } from './authentication.controller';
-import { AuthenticationService } from './authentication.service';
-import { JwtStrategy } from './jwt.strategy';
-import { LocalStrategy } from './local.strategy';
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { JwtModule, JwtService } from "@nestjs/jwt";
+import { PassportModule } from "@nestjs/passport";
+import { UserModule } from "src/modules/user/user.module";
+import { AuthenticationController } from "./authentication.controller";
+import { AuthenticationService } from "./authentication.service";
+import { JwtStrategy } from "./jwt.strategy";
+import { LocalStrategy } from "./local.strategy";
+import { JwtRefreshTokenStrategy } from "./jwt-refresh-token.strategy";
 
 @Module({
   imports: [UserModule, PassportModule, ConfigModule, JwtModule.register({})],
-  providers: [AuthenticationService, LocalStrategy, JwtStrategy, JwtService],
+  providers: [
+    AuthenticationService,
+    LocalStrategy,
+    JwtStrategy,
+    JwtService,
+    JwtRefreshTokenStrategy,
+  ],
   controllers: [AuthenticationController],
 })
 export class AuthenticationModule {}
