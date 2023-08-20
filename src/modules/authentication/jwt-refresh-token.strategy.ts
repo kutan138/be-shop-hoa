753db715ab7ed5977c73f79156e217ceb625e5a0
[CORIZE] Tan Le <tan.b4pt.le@misumi.co.jp>
2023-08-20 11:15:27 +0700

    Update refresh token

--- src/modules/authentication/jwt-refresh-token.strategy.ts
+++ src/modules/authentication/jwt-refresh-token.strategy.ts
@@ -1,26 +1,26 @@
-import { ExtractJwt, Strategy } from 'passport-jwt';
-import { PassportStrategy } from '@nestjs/passport';
-import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import { Request } from 'express';
-import { UserService } from '../user/user.service';
+import { ExtractJwt, Strategy } from "passport-jwt";
+import { PassportStrategy } from "@nestjs/passport";
+import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { Request } from "express";
+import { UserService } from "../user/user.service";
 
 @Injectable()
 export class JwtRefreshTokenStrategy extends PassportStrategy(
   Strategy,
-  'jwt-refresh-token',
+  "jwt-refresh-token"
 ) {
   constructor(
     private readonly configService: ConfigService,
-    private readonly userService: UserService,
+    private readonly userService: UserService
   ) {
     super({
       jwtFromRequest: ExtractJwt.fromExtractors([
         (request: Request) => {
-          return request?.cookies?.Refresh;
+          return request.body.refreshToken;
         },
       ]),
-      secretOrKey: configService.get('JWT_REFRESH_TOKEN_SECRET'),
+      secretOrKey: configService.get("JWT_REFRESH_TOKEN_SECRET"),
       passReqToCallback: true,
     });
   }
@@ -29,7 +29,7 @@ export class JwtRefreshTokenStrategy extends PassportStrategy(
     const refreshToken = request.cookies?.Refresh;
     return this.userService.getUserIfRefreshTokenMatches(
       refreshToken,
-      payload.userId,
+      payload.userId
     );
   }
 }
